d4116fc615c89352eccebf532369a8b7a8e1c842
Nihal Panday <nihalpanday2020@gmail.com>
2025-03-24 09:33:43 +0530

    api for getcourse and getall courses

--- server/controllers/course.controllers.ts
+++ server/controllers/course.controllers.ts
@@ -53,3 +53,33 @@ export const editCourse = catchAsyncError(async (req: Request, res: Response, ne
         return next(new ErrorHandler(error.message, 500));
     }
 });
+
+
+export const getSingleCourse = catchAsyncError(async(req:Request,res:Response,next: NextFunction) => {
+    try {
+        const course = await CourseModel.findById(req.params.id).select("-courseData.videoUrl -courseData.suggestion -courseData.questions -courseData.links")
+
+        res.status(200).json({
+            success : true,
+            course
+        })
+
+    } catch (error : any) {
+        return next(new ErrorHandler(error.message,500))        
+    }
+})
+
+
+export const getAllCourses = catchAsyncError(async(req:Request,res:Response,next:NextFunction) => {
+    try {
+        const courses = await CourseModel.find().select("-courseData.videoUrl -courseData.suggestion -courseData.questions -courseData.links")
+
+        res.status(200).json({
+            success : true,
+            courses,
+        })
+
+    } catch (error :any) {
+       return next(new ErrorHandler(error.message,500)) 
+    }
+})
--- server/routes/course.routes.ts
+++ server/routes/course.routes.ts
@@ -1,11 +1,13 @@
 import express from "express"
-import { uploadCourse } from "../controllers/course.controllers.ts"
-import {isAuthenticated,authorization} from "../middleware/auth.ts"
+import { getAllCourses, getSingleCourse, uploadCourse } from "../controllers/course.controllers.ts"
+import { isAuthenticated, authorization } from "../middleware/auth.ts"
 
 const router = express.Router()
 
-router.post("/create-course",isAuthenticated,authorization("admin"),uploadCourse)
-router.put("/edit-course/:id",isAuthenticated,authorization("admin"),uploadCourse)
+router.post("/create-course", isAuthenticated, authorization("admin"), uploadCourse)
+router.put("/edit-course/:id", isAuthenticated, authorization("admin"), uploadCourse)
+router.get("/get-course/:id", getSingleCourse)
+router.get("/get-courses", getAllCourses)
 
 
 export default router
